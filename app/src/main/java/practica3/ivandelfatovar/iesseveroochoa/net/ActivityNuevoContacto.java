e60f2abd465c8338fde695039ed447ff9e13cb6b
ivand <ivandelfa@gmail.com>
2021-10-19 17:33:24 +0200

    Finalizada la Actividad Principal

---
+++ app/src/main/java/practica3/ivandelfatovar/iesseveroochoa/net/ActivityNuevoContacto.java
@@ -0,0 +1,14 @@
+package practica3.ivandelfatovar.iesseveroochoa.net;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityNuevoContacto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_contacto);
+    }
+}
